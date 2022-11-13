d761f3653343d598a9902f3607264450cde93a60
Da0hn <gabrielhondacba@gmail.com>
2022-11-13 10:10:30 -0400

    feat: add `OrderDomainService` interface

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/service/OrderDomainService.java
@@ -0,0 +1,33 @@
+package com.food.ordering.system.domain.service;
+
+import com.food.ordering.system.domain.entity.Order;
+import com.food.ordering.system.domain.entity.Restaurant;
+import com.food.ordering.system.domain.event.OrderCancelledEvent;
+import com.food.ordering.system.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.domain.event.OrderPaidEvent;
+
+import java.util.List;
+
+public interface OrderDomainService {
+
+  OrderCreatedEvent validateAndInitiateOrder(
+    Order order,
+    Restaurant restaurant
+  );
+
+  OrderPaidEvent payOrder(Order order);
+
+  void approveOrder(Order order);
+
+  OrderCancelledEvent cancelOrderPayment(
+    Order order,
+    List<String> failureMessages
+  );
+
+  void cancelOrder(
+    Order order,
+    List<String> failureMessages
+  );
+
+
+}
