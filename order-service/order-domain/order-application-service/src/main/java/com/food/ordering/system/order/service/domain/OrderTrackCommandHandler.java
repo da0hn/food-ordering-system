15c1fa15e23f5e019323eb3b1a67c4a544649cce
Da0hn <gabrielhondacba@gmail.com>
2022-11-14 20:39:16 -0400

    wip: create `OrderTrackCommandHandler` component

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -0,0 +1,16 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class OrderTrackCommandHandler {
+
+  TrackOrderResponse handle(final TrackOrderQuery query) {
+    return null;
+  }
+
+}
