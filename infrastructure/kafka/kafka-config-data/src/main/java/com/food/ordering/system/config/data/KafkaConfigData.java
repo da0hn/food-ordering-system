4ce1053e7f87e295bc29b73d27b243fbf0098377
Da0hn <gabrielhondacba@gmail.com>
2022-11-19 13:08:07 -0400

    feat: add kafka configuration data

---
+++ infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/config/data/KafkaConfigData.java
@@ -0,0 +1,18 @@
+package com.food.ordering.system.config.data;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka-config")
+public class KafkaConfigData {
+
+  private String bootstrapServers;
+  private String schemaRegistryUrlKey;
+  private String schemaRegistryUrl;
+  private Integer numOfPartitions;
+  private Short replicationFactor;
+
+}
