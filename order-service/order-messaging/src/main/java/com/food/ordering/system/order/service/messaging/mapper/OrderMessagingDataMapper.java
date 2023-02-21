943121ec4a62e78927c8e9e311e1a1bf16df6341
Da0hn <gabrielhondacba@gmail.com>
2023-02-21 12:26:54 -0400

    feat: add mapper of `OrderPaidEvent` to `RestaurantApprovalRequestAvroModel`

--- order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/mapper/OrderMessagingDataMapper.java
@@ -1,18 +1,22 @@
 package com.food.ordering.system.order.service.messaging.mapper;
 
-import com.food.ordering.system.domain.entity.Order;
 import com.food.ordering.system.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.domain.event.OrderPaidEvent;
 import com.food.ordering.system.kafka.order.avro.model.PaymentOrderStatus;
 import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.kafka.order.avro.model.Product;
+import com.food.ordering.system.kafka.order.avro.model.RestaurantApprovalRequestAvroModel;
+import com.food.ordering.system.kafka.order.avro.model.RestaurantOrderStatus;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class OrderMessagingDataMapper {
 
-  public PaymentRequestAvroModel orderCreatedEventToPaymentRequestAvroModel(OrderCreatedEvent event) {
+  public PaymentRequestAvroModel orderCreatedEventToPaymentRequestAvroModel(final OrderCreatedEvent event) {
     final var order = event.getOrder();
     return PaymentRequestAvroModel.newBuilder()
       .setId(UUID.randomUUID().toString())
@@ -26,7 +30,7 @@ public class OrderMessagingDataMapper {
   }
 
 
-  public PaymentRequestAvroModel orderCancelledEventToPaymentRequestAvroModel(OrderCancelledEvent event) {
+  public PaymentRequestAvroModel orderCancelledEventToPaymentRequestAvroModel(final OrderCancelledEvent event) {
     final var order = event.getOrder();
     return PaymentRequestAvroModel.newBuilder()
       .setId(UUID.randomUUID().toString())
@@ -38,5 +42,29 @@ public class OrderMessagingDataMapper {
       .setPaymentOrderStatus(PaymentOrderStatus.CANCELLED)
       .build();
   }
+
+  public RestaurantApprovalRequestAvroModel orderPaidEventToRestaurantApprovalRequestAvroModel(final OrderPaidEvent event) {
+    final var order = event.getOrder();
+    return RestaurantApprovalRequestAvroModel.newBuilder()
+      .setId(UUID.randomUUID().toString())
+      .setSagaId("")
+      .setOrderId(order.getId().getValue().toString())
+      .setRestaurantId(order.getRestaurantId().getValue().toString())
+      .setRestaurantOrderStatus(RestaurantOrderStatus.valueOf(order.getOrderStatus().name()))
+      .setPrice(order.getPrice().getAmount())
+      .setCreatedAt(event.getCreatedAt().toInstant())
+      .setRestaurantOrderStatus(RestaurantOrderStatus.PAID)
+      .setProducts(
+        order.getItems().stream()
+          .map(item -> Product.newBuilder()
+            .setId(item.getId().getValue().toString())
+            .setQuantity(item.getQuantity())
+            .build()
+          )
+          .collect(Collectors.toList())
+      )
+      .build();
+  }
+
 
 }
