228c128ee93120acb3e78270d4c4e547baaeff05
Da0hn <gabrielhondacba@gmail.com>
2023-02-21 16:32:38 -0400

    fix: change `List<Long>` of `Kafka` keys to `List<String>`

--- infrastructure/kafka/kafka-consumer/src/main/java/com/food/ordering/system/kafka/consumer/KafkaConsumer.java
+++ infrastructure/kafka/kafka-consumer/src/main/java/com/food/ordering/system/kafka/consumer/KafkaConsumer.java
@@ -9,7 +9,7 @@ public interface KafkaConsumer<T extends SpecificRecordBase> {
 
   void receive(
     List<T> message,
-    List<Long> keys,
+    List<String> keys,
     List<Integer> partitions,
     List<Long> offsets
   );
