c6bdedb60c066eb4556331551a1bd97d1b02ae1c
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-02-21 10:52:55 -0400

    feat: add `Product` builder implementation

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Product.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Product.java
@@ -18,8 +18,18 @@ public class Product extends BaseEntity<ProductId> {
     this.price = price;
   }
 
+  private Product(final Builder builder) {
+    super.setId(builder.id);
+    this.name = builder.name;
+    this.price = builder.price;
+  }
+
   public static Product of(final ProductId productId) {
     return new Product(productId, null, null);
+  }
+
+  public static Product.Builder builder() {
+    return new Product.Builder();
   }
 
   public String getName() {
@@ -38,13 +48,45 @@ public class Product extends BaseEntity<ProductId> {
     this.price = price;
   }
 
-
   public void updateWithConfirmedNameAndPrice(
     final String name,
     final Money price
   ) {
     this.name = name;
     this.price = price;
+  }
+
+  public static final class Builder {
+
+    private ProductId id;
+    private String name;
+    private Money price;
+
+    private Builder() {}
+
+    public static Builder builder() {
+      return new Builder();
+    }
+
+    public Builder id(final ProductId id) {
+      this.id = id;
+      return this;
+    }
+
+    public Builder name(final String name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder price(final Money price) {
+      this.price = price;
+      return this;
+    }
+
+    public Product build() {
+      return new Product(this);
+    }
+
   }
 
 }
