880de10bcbf8559074cf591289873a61f0f699cd
Gabriel Honda <gabriel.honda@totemti.com.br>
2023-02-21 08:56:03 -0400

    fix: add constructor for `AggregateRoot` and `Customer`
    - Constructor receiving generic `ID`
    - Empty constructor

--- common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
@@ -1,4 +1,12 @@
 package com.food.ordering.system.domain.entity;
 
 public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
+
+  protected AggregateRoot(final ID id) {
+    super(id);
+  }
+
+  protected AggregateRoot() {
+  }
+
 }
--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Customer.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Customer.java
@@ -3,4 +3,12 @@ package com.food.ordering.system.domain.entity;
 import com.food.ordering.system.domain.valueobject.CustomerId;
 
 public class Customer extends AggregateRoot<CustomerId> {
+
+  public Customer() {
+  }
+
+  public Customer(final CustomerId customerId) {
+    super(customerId);
+  }
+
 }
