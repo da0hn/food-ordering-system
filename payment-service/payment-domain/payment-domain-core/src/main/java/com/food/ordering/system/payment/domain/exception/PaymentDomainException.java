4fc48065606b4f5db2627017f19174ae37b72041
Da0hn <gabrielhondacba@gmail.com>
2023-06-08 18:01:22 -0400

    fix: add field `serialVersionUID`

--- payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/domain/exception/PaymentDomainException.java
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/domain/exception/PaymentDomainException.java
@@ -2,8 +2,12 @@ package com.food.ordering.system.payment.domain.exception;
 
 import com.food.ordering.system.domain.exception.DomainException;
 
+import java.io.Serial;
+
 public class PaymentDomainException extends DomainException {
 
+  @Serial
+  private static final long serialVersionUID = 4682770145257011186L;
 
   public PaymentDomainException(final String message) {
     super(message);
--- payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/domain/exception/PaymentNotFoundException.java
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/domain/exception/PaymentNotFoundException.java
@@ -2,7 +2,12 @@ package com.food.ordering.system.payment.domain.exception;
 
 import com.food.ordering.system.domain.exception.DomainException;
 
+import java.io.Serial;
+
 public class PaymentNotFoundException extends DomainException {
+  @Serial
+  private static final long serialVersionUID = -5157159403861902090L;
+
   public PaymentNotFoundException(final String message) {
     super(message);
   }
