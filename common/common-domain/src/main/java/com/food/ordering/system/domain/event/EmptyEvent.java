512868c7332f76743116e2942be93eb5d10b1134
Da0hn <gabrielhondacba@gmail.com>
2023-06-18 09:12:17 -0400

    feat: add `EmptyEvent` implementation

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/event/EmptyEvent.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.domain.event;
+
+public final class EmptyEvent implements DomainEvent<Void> {
+
+  public static final EmptyEvent INSTANCE = new EmptyEvent();
+
+  private EmptyEvent() {}
+}
