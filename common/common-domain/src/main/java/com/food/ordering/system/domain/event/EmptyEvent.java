fa57956cef57806e4e63747de5d1890f4d15551d
Da0hn <gabrielhondacba@gmail.com>
2023-06-20 21:45:21 -0400

    feat: get `EmptyEvent` instance using static method

--- common/common-domain/src/main/java/com/food/ordering/system/domain/event/EmptyEvent.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/event/EmptyEvent.java
@@ -2,9 +2,13 @@ package com.food.ordering.system.domain.event;
 
 public final class EmptyEvent implements DomainEvent<Void> {
 
-  public static final EmptyEvent INSTANCE = new EmptyEvent();
+  private static final EmptyEvent INSTANCE = new EmptyEvent();
 
   private EmptyEvent() {}
+
+  public static EmptyEvent empty() {
+    return INSTANCE;
+  }
 
   @Override
   public void fire() {}
