8bc0cf27f8ced0159626ba33d84f48990df12b9e
Da0hn <gabrielhondacba@gmail.com>
2023-06-20 22:45:09 -0400

    fix: adjust `init-schema.sql` of `restaurant-service`

--- restaurant-service/restaurant-container/src/main/resources/init-schema.sql
+++ restaurant-service/restaurant-container/src/main/resources/init-schema.sql
@@ -8,24 +8,24 @@ DROP TABLE IF EXISTS restaurant.restaurants CASCADE;
 
 CREATE TABLE restaurant.restaurants
 (
-    id     uuid                                           NOT NULL,
-    name   character varying COLLATE pg_catalog."default" NOT NULL,
-    active boolean                                        NOT NULL,
+    id uuid NOT NULL,
+    name character varying COLLATE pg_catalog."default" NOT NULL,
+    active boolean NOT NULL,
     CONSTRAINT restaurants_pkey PRIMARY KEY (id)
-)
+);
 
 DROP TYPE IF EXISTS approval_status;
 
-CREATE TYPE approval_status as ENUM ('APPROVED', 'REJECTED');
+CREATE TYPE approval_status AS ENUM ('APPROVED', 'REJECTED');
 
 DROP TABLE IF EXISTS restaurant.order_approval CASCADE;
 
 CREATE TABLE restaurant.order_approval
 (
-    id            uuid            NOT NULL,
-    restaurant_id uuid            NOT NULL,
-    order_id      uuid            NOT NULL,
-    status        approval_status NOT NULL,
+    id uuid NOT NULL,
+    restaurant_id uuid NOT NULL,
+    order_id uuid NOT NULL,
+    status approval_status NOT NULL,
     CONSTRAINT order_approval_pkey PRIMARY KEY (id)
 );
 
@@ -33,10 +33,10 @@ DROP TABLE IF EXISTS restaurant.products CASCADE;
 
 CREATE TABLE restaurant.products
 (
-    id        uuid                                           NOT NULL,
-    name      character varying COLLATE pg_catalog."default" NOT NULL,
-    price     numeric(10, 2)                                 NOT NULL,
-    available boolean                                        NOT NULL,
+    id uuid NOT NULL,
+    name character varying COLLATE pg_catalog."default" NOT NULL,
+    price numeric(10,2) NOT NULL,
+    available boolean NOT NULL,
     CONSTRAINT products_pkey PRIMARY KEY (id)
 );
 
@@ -44,9 +44,9 @@ DROP TABLE IF EXISTS restaurant.restaurant_products CASCADE;
 
 CREATE TABLE restaurant.restaurant_products
 (
-    id            uuid NOT NULL,
+    id uuid NOT NULL,
     restaurant_id uuid NOT NULL,
-    product_id    uuid NOT NULL,
+    product_id uuid NOT NULL,
     CONSTRAINT restaurant_products_pkey PRIMARY KEY (id)
 );
 
@@ -69,39 +69,35 @@ DROP MATERIALIZED VIEW IF EXISTS restaurant.order_restaurant_m_view;
 CREATE MATERIALIZED VIEW restaurant.order_restaurant_m_view
     TABLESPACE pg_default
 AS
-SELECT r.id        AS restaurant_id,
-       r.name      AS restaurant_name,
-       r.active    AS restaurant_active,
-       p.id        AS product_id,
-       p.name      AS product_name,
-       p.price     AS product_price,
+SELECT r.id AS restaurant_id,
+       r.name AS restaurant_name,
+       r.active AS restaurant_active,
+       p.id AS product_id,
+       p.name AS product_name,
+       p.price AS product_price,
        p.available AS product_available
 FROM restaurant.restaurants r,
      restaurant.products p,
      restaurant.restaurant_products rp
-WHERE r.id = rp.restaurant_id
-  AND p.id = rp.product_id
+WHERE r.id = rp.restaurant_id AND p.id = rp.product_id
 WITH DATA;
 
-REFRESH MATERIALIZED VIEW restaurant.order_restaurant_m_view;
+refresh materialized VIEW restaurant.order_restaurant_m_view;
 
-DROP FUNCTION IF EXISTS restaurant.refresh_order_restaurant_m_view;
+DROP function IF EXISTS restaurant.refresh_order_restaurant_m_view;
 
-CREATE OR REPLACE FUNCTION restaurant.refresh_order_restaurant_m_view()
-    RETURNS TRIGGER
-AS
-'
+CREATE OR replace function restaurant.refresh_order_restaurant_m_view()
+    returns trigger
+AS '
     BEGIN
-        refresh materialized view restaurant.order_restaurant_m_view;
+        refresh materialized VIEW restaurant.order_restaurant_m_view;
         return null;
-    end;
-' LANGUAGE plpgsql;
+    END;
+'  LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS refresh_order_restaurant_m_view ON restaurant.restaurant_products;
+DROP trigger IF EXISTS refresh_order_restaurant_m_view ON restaurant.restaurant_products;
 
-CREATE TRIGGER refresh_order_restaurant_m_view
-    AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
-    ON restaurant.restaurant_products
-    FOR EACH STATEMENT
-EXECUTE PROCEDURE
-    restaurant.refresh_order_restaurant_m_view();
+CREATE trigger refresh_order_restaurant_m_view
+    after INSERT OR UPDATE OR DELETE OR truncate
+    ON restaurant.restaurant_products FOR each statement
+EXECUTE PROCEDURE restaurant.refresh_order_restaurant_m_view();
