9e7ffdaa37b2979c5cbfea7f27d4dbab175ee7bd
Da0hn <gabrielhondacba@gmail.com>
2023-06-20 21:49:29 -0400

    feat: add Order Payment Saga implementation

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -1,5 +1,6 @@
 package com.food.ordering.system.order.service.domain;
 
+import com.food.ordering.system.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
 import com.food.ordering.system.order.service.domain.ports.api.message.listener.PaymentResponseMessageListener;
 import lombok.AllArgsConstructor;
@@ -13,14 +14,23 @@ import org.springframework.validation.annotation.Validated;
 @AllArgsConstructor
 public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
 
+  private final OrderPaymentSaga orderPaymentSaga;
+
   @Override
   public void paymentCompleted(final PaymentResponse response) {
-
+    final var event = this.orderPaymentSaga.process(response);
+    log.info("Publishing OrderPaidEvent for order id: {} ", response.getOrderId());
+    event.fire();
   }
 
   @Override
   public void paymentCancelled(final PaymentResponse response) {
-
+    final var event = this.orderPaymentSaga.rollback(response);
+    log.info(
+      "Order is rolled back for order id: {} with failure messages: {}",
+      response.getOrderId(),
+      String.join(Order.FAILURE_MESSAGE_DELIMITER, response.getFailureMessages())
+    );
   }
 
 }
