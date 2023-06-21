245b89032aa58f08f51d555542f6393aad80e0e9
Da0hn <gabrielhondacba@gmail.com>
2023-06-20 23:04:20 -0400

    chore: adjust initial data consistency with another services

--- customer-service/src/main/resources/init-data.sql
+++ customer-service/src/main/resources/init-data.sql
@@ -1,2 +1,3 @@
-INSERT INTO customer.customers(id, username, first_name, last_name) VALUES ('1aaea062-ea1f-4a52-bf62-ab6df79c4bb3', 'user_1', 'First', 'User')
-INSERT INTO customer.customers(id, username, first_name, last_name) VALUES ('d5206e4e-388b-4eca-89d6-2c7a679baee7', 'user_1', 'Second', 'User')
+-- @formatter:off
+INSERT INTO customer.customers(id, username, first_name, last_name) VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb41', 'user_1', 'First', 'User');
+INSERT INTO customer.customers(id, username, first_name, last_name) VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb42', 'user_2', 'Second', 'User');
